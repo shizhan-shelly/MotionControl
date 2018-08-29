942437d5a5e37e3925e20a60cf854fab8ed61a12
zhanshi <shizhan-shelly@hotmail.com>
2018-08-29 20:22:51 +0800

    Implement Cutter::StartupModbusClient().

--- src/MotionControl/Cutter.cpp
+++ src/MotionControl/Cutter.cpp
@@ -3,9 +3,22 @@
 
 #include "MotionControl/Cutter.h"
 
-#include "MotionControl/Communication/Modbus/ClientHandler.h"
+#include "MotionControl/Communication/Modbus/RTUClientHandler.h"
 
-Cutter::Cutter() {}
+Cutter::Cutter() : client_handler_(NULL) {}
 
 Cutter::~Cutter() {}
 
+MasterClient *Cutter::modbus_client() {
+  return &modbus_client_;
+}
+
+bool Cutter::StartupModbusClient() {
+  if (client_handler_) {
+    delete client_handler_;
+    client_handler_ = NULL;
+  }
+  client_handler_ = new RTUClientHandler("COM1", 19200, 'E', 8, 1, 1);
+  modbus_client_.SetMasterClientHandler(client_handler_);
+  return modbus_client_.SetupConnect();
+}
--- src/MotionControl/Cutter.h
+++ src/MotionControl/Cutter.h
@@ -5,15 +5,24 @@
 #define CUTTER_H__
 
 #include "base/singleton/Singleton.h"
+#include "MotionControl/Communication/Modbus/MasterClient.h"
+
+class ClientHandler;
 
 class Cutter : public Singleton {
   DECLARE_SINGLETON(Cutter)
 
  public:
+  MasterClient *modbus_client();
+
+  bool StartupModbusClient();
 
  private:
   Cutter();
   ~Cutter();
+
+  ClientHandler *client_handler_;
+  MasterClient modbus_client_;
 
 }; //class Cutter
 
