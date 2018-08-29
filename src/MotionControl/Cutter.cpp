fd5dab6433c428ee2147daa8bfd23c1069b93263
zhanshi <shizhan-shelly@hotmail.com>
2018-08-29 16:57:17 +0800

    Design Cutter.

---
+++ src/MotionControl/Cutter.cpp
@@ -0,0 +1,11 @@
+// Copyright 2018 Fangling Software Co., Ltd. All Rights Reserved.
+// Author: shizhan-shelly@hotmail.com (Zhan Shi)
+
+#include "MotionControl/Cutter.h"
+
+#include "MotionControl/Communication/Modbus/ClientHandler.h"
+
+Cutter::Cutter() {}
+
+Cutter::~Cutter() {}
+
---
+++ src/MotionControl/Cutter.h
@@ -0,0 +1,20 @@
+// Copyright 2018 Fangling Software Co., Ltd. All Rights Reserved.
+// Author: shizhan-shelly@hotmail.com (Zhan Shi)
+
+#ifndef CUTTER_H__
+#define CUTTER_H__
+
+#include "base/singleton/Singleton.h"
+
+class Cutter : public Singleton {
+  DECLARE_SINGLETON(Cutter)
+
+ public:
+
+ private:
+  Cutter();
+  ~Cutter();
+
+}; //class Cutter
+
+#endif // CUTTER_H__
