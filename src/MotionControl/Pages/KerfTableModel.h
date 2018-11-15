027e6501598656d4dfcf0662d2533982f2687447
zhanshi <shizhan-shelly@hotmail.com>
2018-11-15 19:49:42 +0800

    Design KerfTableModel.

---
+++ src/MotionControl/Pages/KerfTableModel.h
@@ -0,0 +1,32 @@
+// Copyright 2018 Fangling Software Co., Ltd. All Rights Reserved.
+// Author: shizhan-shelly@hotmail.com (Zhan Shi)
+
+#ifndef KERFTABLEMODEL_H__
+#define KERFTABLEMODEL_H__
+
+#include <QtCore/QAbstractTableModel>
+
+class KerfTableModel : public QAbstractTableModel {
+  Q_OBJECT
+
+ public:
+  explicit KerfTableModel(QObject *parent = NULL);
+  ~KerfTableModel();
+
+  int rowCount(const QModelIndex &parent = QModelIndex()) const;
+  int columnCount(const QModelIndex &parent = QModelIndex()) const;
+
+  Qt::ItemFlags flags(const QModelIndex &index) const;
+
+  QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+  QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+
+  // The dataChanged() signal should be emitted if the data was successfully set.
+  bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole);
+
+ private:
+  QMap<int, double> kerf_variable_value_;
+
+}; // class KerfTableModel
+
+#endif // KERFTABLEMODEL_H__
