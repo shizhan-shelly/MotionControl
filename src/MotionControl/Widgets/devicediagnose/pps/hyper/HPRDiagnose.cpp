0494839dc80e20eaa2e9869b1f412cf1780363bc
ZhanShi <shizhan-shelly@hotmail.com>
2020-02-19 23:03:57 +0800

    Design HPRDiagnose widget.

--- src/MotionControl/Widgets/devicediagnose/pps/hyper/HPRDiagnose.cpp
+++ src/MotionControl/Widgets/devicediagnose/pps/hyper/HPRDiagnose.cpp
@@ -1,15 +1,56 @@
+// Copyright 2020 Fangling Software Co., Ltd. All Rights Reserved.
+// Author: shizhan-shelly@hotmail.com (Zhan Shi)
+
 #include "HPRDiagnose.h"
 
 #include "ui_HPRDiagnose.h"
 
+namespace cutter {
+
 HPRDiagnose::HPRDiagnose(QWidget *parent)
-    : QWidget(parent) {
+    : QWidget(parent)
+    , station_id_(0) {
 
   ui_ = new Ui::HPRDiagnose();
   ui_->setupUi(this);
+  setStyleSheet("QPushButton, QLabel, QComboBox, QLineEdit { font-size: 11pt;\
+                 font-family:SimSun }");
+
+  initialDiagnoseWidget();
 }
 
 HPRDiagnose::~HPRDiagnose() {
   delete ui_;
 }
 
+void HPRDiagnose::initialDiagnoseWidget() {
+  connect(ui_->preflow_gas_test_, SIGNAL(clicked()), this, SLOT(onPreflowGasTest()));
+  connect(ui_->cutflow_gas_test_, SIGNAL(clicked()), this, SLOT(onCutflowGasTest()));
+  connect(ui_->manual_pump_ctrl_, SIGNAL(clicked()), this, SLOT(onManualPumpCtrl()));
+  connect(ui_->set_btn_, SIGNAL(clicked()), this, SLOT(onSetParameter()));
+  connect(ui_->clear_pps_record_, SIGNAL(clicked()), this, SLOT(onClearRecord()));
+}
+
+void HPRDiagnose::setCurrentStation(int station_id) {
+  station_id_ = station_id;
+}
+
+void HPRDiagnose::Update() {
+}
+
+void HPRDiagnose::onPreflowGasTest() {
+}
+
+void HPRDiagnose::onCutflowGasTest() {
+}
+
+void HPRDiagnose::onManualPumpCtrl() {
+}
+
+void HPRDiagnose::onSetParameter() {
+}
+
+void HPRDiagnose::onClearRecord() {
+}
+
+} // namespace cutter
--- src/MotionControl/Widgets/devicediagnose/pps/hyper/HPRDiagnose.h
+++ src/MotionControl/Widgets/devicediagnose/pps/hyper/HPRDiagnose.h
@@ -1,22 +1,43 @@
-#ifndef HPRDIAGNOSE_H
-#define HPRDIAGNOSE_H
+// Copyright 2020 Fangling Software Co., Ltd. All Rights Reserved.
+// Author: shizhan-shelly@hotmail.com (Zhan Shi)
+
+#ifndef CUTTER_WIDGET_DEVICEDIAGNOSE_PPS_HYPER_HPRDIAGNOSE_H__
+#define CUTTER_WIDGET_DEVICEDIAGNOSE_PPS_HYPER_HPRDIAGNOSE_H__
 
 #include <QWidget>
 
 namespace Ui {
 class HPRDiagnose;
-};
+}
+
+namespace cutter {
 
 class HPRDiagnose : public QWidget {
   Q_OBJECT
 
  public:
   explicit HPRDiagnose(QWidget *parent = NULL);
-  virtual ~HPRDiagnose();
+  ~HPRDiagnose();
+
+  void setCurrentStation(int station_id);
+
+  void Update();
+
+ private slots:
+  void onPreflowGasTest();
+  void onCutflowGasTest();
+  void onManualPumpCtrl();
+  void onSetParameter();
+  void onClearRecord();
 
  private:
+  int station_id_;
   Ui::HPRDiagnose *ui_;
+
+  void initialDiagnoseWidget();
 
 }; // class HPRDiagnose
 
-#endif // HPRDIAGNOSE_H
+} // namespace cutter
+
+#endif // CUTTER_WIDGET_DEVICEDIAGNOSE_PPS_HYPER_HPRDIAGNOSE_H__
