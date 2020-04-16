f961cc1aecbbfa20b4c6b2f2d95828dba27ce192
zhanshi <shizhan-shelly@hotmail.com>
2020-04-16 19:53:05 +0800

    Design IOConfigObserver.

---
+++ src/MotionControl/Controller/board/IOConfigObserver.cpp
@@ -0,0 +1,62 @@
+// Copyright 2020 Fangling Software Co., Ltd. All Rights Reserved.
+// Author: shizhan-shelly@hotmail.com (Zhan Shi)
+
+#include "MotionControl/Controller/board/IOConfigObserver.h"
+
+#include "MotionControl/Controller/board/AbstractBoard.h"
+#include "MotionControl/Controller/board/IOBoardManager.h"
+
+IOConfigObserver::IOConfigObserver() {}
+
+IOConfigObserver::~IOConfigObserver() {}
+
+std::vector<int> IOConfigObserver::GetConfigurableGroup() const {
+  std::vector<int> group;
+  for (int i = 0; i < BOARD_SIZE; i++) {
+    group.push_back(i);
+  }
+  return group;
+}
+
+std::vector<int> IOConfigObserver::GetConfigurableIndex(
+    int group,
+    int io_type) const {
+
+  int cnt = 0;
+  std::map<int, AbstractBoard *> boards = IOBoardManager::GetInstance()->GetBoard();
+  std::map<int, AbstractBoard *>::iterator it = boards.find(group);
+  AbstractBoard *board = it != boards.end() ? it->second : NULL;
+  if (board) {
+    switch (io_type) {
+     case 0:
+      cnt = board->GetBoardInputSize();
+      break;
+     case 1:
+      cnt = board->GetBoardOutputSize();
+      break;
+     case 2:
+      cnt = board->GetBoardADSize();
+      break;
+     case 3:
+      cnt = board->GetBoardDASize();
+      break;
+     default:
+      break;
+    }
+  }
+  std::vector<int> index;
+  for (int i = 0; i <= cnt; i++) {
+    index.push_back(i);
+  }
+  return index;
+}
+
+QStringList IOConfigObserver::TransformQStringList(
+    const std::vector<int> &items) {
+
+  QStringList list;
+  for (size_t i = 0; i < items.size(); i++) {
+    list << QString("%1").arg(items[i]);
+  }
+  return list;
+}
---
+++ src/MotionControl/Controller/board/IOConfigObserver.h
@@ -0,0 +1,29 @@
+// Copyright 2020 Fangling Software Co., Ltd. All Rights Reserved.
+// Author: shizhan-shelly@hotmail.com (Zhan Shi)
+
+#ifndef CONTROLLER_BOARD_IOCONFIGOBSERVER_H__
+#define CONTROLLER_BOARD_IOCONFIGOBSERVER_H__
+
+#include "base/singleton/Singleton.h"
+
+#include <vector>
+#include <QStringList>
+
+class IOConfigObserver : public Singleton {
+  DECLARE_SINGLETON(IOConfigObserver)
+
+ public:
+  std::vector<int> GetConfigurableGroup() const;
+
+  // io_type: 0, input; 1, output; 2, AD; 3, DA
+  std::vector<int> GetConfigurableIndex(int group, int io_type) const;
+
+  static QStringList TransformQStringList(const std::vector<int> &items);
+
+ private:
+  IOConfigObserver();
+  ~IOConfigObserver();
+
+}; // class IOConfigObserver
+
+#endif // CONTROLLER_BOARD_IOCONFIGOBSERVER_H__
